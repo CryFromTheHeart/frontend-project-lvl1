fe8c6cbf978206d752db7a3a8f5b2af638fffb58
Никита Машков <nik.mashcov13@Mail.ru>
2021-10-21 18:18:13 +0400

    fix src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const toGreet = () => {
-	const name = readlineSync.question('May i have your name? ');
-	console.log(`Hello, ${name}!`);
+const toGreet = () => {
+  const name = readlineSync.question('May i have your name? ');
+  console.log(`Hello, ${name}!`);
 };
+
+export default toGreet;
