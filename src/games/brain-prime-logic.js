911cd6b3d63b64d3b3a06d357e685f613fa31aee
Никита Машков <nik.mashcov13@Mail.ru>
2021-11-03 18:55:06 +0400

    fix src/games/brain-prime-logic.js

--- src/games/brain-prime-logic.js
+++ src/games/brain-prime-logic.js
@@ -4,6 +4,7 @@ import getRandomInt from '../getRandomInt.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  if (num === 1) return false;
   for (let divisor = 2; divisor <= num / 2; divisor += 1) {
     if (num % divisor === 0) {
       return false;
@@ -13,7 +14,7 @@ const isPrime = (num) => {
 };
 
 const getData = () => {
-  const number = getRandomInt(0, 100);
+  const number = getRandomInt(1, 100);
   const question = (`${number}`);
   const correctAnswer = (isPrime(number)) ? 'yes' : 'no';
   return [correctAnswer, question];
