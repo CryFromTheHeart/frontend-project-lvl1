3fa2a40a94f75997b6037daceaac738c09bb6497
Никита Машков <nik.mashcov13@Mail.ru>
2021-11-03 18:39:59 +0400

    fix src/games/brain-progression.js

--- src/games/brain-progression-logic.js
+++ src/games/brain-progression-logic.js
@@ -5,25 +5,25 @@ const description = 'What number is missing in the progression?';
 const maxStep = 10;
 
 const getProgression = (firstMemberProgression, progressionStep, numberOfTheGuessNumber) => {
-  let stringNumbers = '';
-  let guessNumber = 0;
+  const progression = [];
   for (let countProgressionStep = 1; countProgressionStep <= maxStep; countProgressionStep += 1) {
     const currentMember = firstMemberProgression + (progressionStep * countProgressionStep);
     if (numberOfTheGuessNumber === countProgressionStep) {
-      guessNumber = currentMember;
-      stringNumbers = `${stringNumbers}.. `;
+      progression.push('..');
     } else {
-      stringNumbers = `${stringNumbers}${currentMember} `;
+      progression.push(`${currentMember}`);
     }
   }
-  return [String(guessNumber), stringNumbers];
+  const progressionToString = progression.join(' ');
+  return progressionToString;
 };
 
 const getData = () => {
   const firstMember = getRandomInt(0, 15);
   const progressionStep = getRandomInt(2, 10);
   const numberGuessNumber = getRandomInt(2, 10);
-  const [guessNumber, question] = getProgression(firstMember, progressionStep, numberGuessNumber);
+  const question = getProgression(firstMember, progressionStep, numberGuessNumber);
+  const guessNumber = String(firstMember + (progressionStep * numberGuessNumber));
   return [guessNumber, question];
 };
 
